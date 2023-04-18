cb2a991626b9fb49ecf8942bad62a9e3131189da
Fumiaki Tou <fumiakitou@Fumiakis-MacBook-Pro.local>
2023-04-18 12:31:13 +0900

    format mjs files

--- server/api-routes/index.mjs
+++ server/api-routes/index.mjs
@@ -2,6 +2,6 @@ import express from "express";
 import booksRouter from "./diaries.mjs";
 
 const router = express.Router();
-router.use("/diaries", booksRouter)
+router.use("/diaries", booksRouter);
 
 export default router;
--- server/models/diary.mjs
+++ server/models/diary.mjs
@@ -1,14 +1,17 @@
 import { Schema, model } from "mongoose";
-const diarySchema = Schema({
-  title: {
-    type: String,
-    required: true
+const diarySchema = Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
   },
-  text: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const diary = model("diary", diarySchema);
 export default diary;

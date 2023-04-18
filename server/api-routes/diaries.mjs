b01fd7078e2c9775d84cebf72ce73bfa9a60af86
Fumiaki Tou <fumiakitou@Fumiakis-MacBook-Pro.local>
2023-04-18 12:19:46 +0900

    fix update api

--- server/api-routes/diaries.mjs
+++ server/api-routes/diaries.mjs
@@ -1,11 +1,11 @@
 import express from "express";
 import { body } from "express-validator";
-import { deleteDiaryId, updateDiary, getDiaryByDate, writeDiary } from "../controllers/diaries.mjs";
+import { updateDiary, deleteDiaryId, getAllDiaries, getDiaryByDate, writeDiary } from "../controllers/diaries.mjs";
 import requestErrorHandler from "../helpers/helper.mjs";
 
 const router = express.Router();
 
-// router.get("/", requestErrorHandler(getAllDiaries));
+router.get("/", requestErrorHandler(getAllDiaries));
 
 router.get("/:id", requestErrorHandler(getDiaryByDate));
 
@@ -15,14 +15,14 @@ router.post(
   body("text").notEmpty().withMessage("error message"),
   requestErrorHandler(writeDiary));
 
-// router.patch(
-//   "/:id",
-//   body("title").optional().notEmpty(),
-//   body("description").optional().notEmpty(),
-//   body("comment").optional().notEmpty(),
-//   body("rating").optional().notEmpty().isInt({ min: 1, max: 5 }),
-//   requestErrorHandler(updateDiary));
+router.patch(
+  "/:id",
+  body("title").optional().notEmpty(),
+  body("description").optional().notEmpty(),
+  body("comment").optional().notEmpty(),
+  body("rating").optional().notEmpty().isInt({ min: 1, max: 5 }),
+  requestErrorHandler(updateDiary));
 
-// router.delete("/:id", requestErrorHandler(deleteDiaryId));
+router.delete("/:id", requestErrorHandler(deleteDiaryId));
 
 export default router;
